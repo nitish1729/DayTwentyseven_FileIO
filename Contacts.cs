9b9053f0af862c915ed45e41382cbdd23e9cecc9
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-16 22:11:10 +0530

    Created Contacts

---
+++ Contacts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addressbook_FileIO
+{
+    public class Contacts
+    {
+       
+            public String FirstName { get; set; }
+            public String LastName { get; set; }
+            public String Address { get; set; }
+            public String CityName { get; set; }
+            public String State { get; set; }
+            public long  ZipCode{ get; set; }
+            public long PhoneNum { get; set; }
+            public String Email_ID { get; set; }
+       
+    }
+}
