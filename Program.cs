770573e136e9b7e61168346a2d3e7ce609ec4b7b
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-16 23:54:30 +0530

    create Contact

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
-﻿string wellcome;
-wellcome = "Welcome to Address Book Program in AddressBookMain class on Master Branch";
-Console.WriteLine("AddressBooks program by File Handling:- {0}", wellcome);
-Console.ReadLine();
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Addressbook_FileIO;
+
+//string wellcome;
+//wellcome = "Welcome to Address Book Program in AddressBookMain class on Master Branch";
+//Console.WriteLine("AddressBooks program by File Handling:- {0}", wellcome);
+//Console.ReadLine();
+AddessBook show = new AddessBook();
+show.ContactDetails();
+show.display();
