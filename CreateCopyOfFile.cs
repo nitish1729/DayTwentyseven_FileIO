ad7ea118e3d47c9c02e1872b0386a7130a47beae
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-17 19:21:38 +0530

    Copy the File

---
+++ CreateCopyOfFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addressbook_FileIO
+{
+    public class CreateCopyOfFile
+    {
+        public void CopyFile()
+        {
+            string path = @"C:\Users\NITISH MEHTA\OneDrive\Desktop\Bridge_Labz\27\Addressbook_FileIO\test.txt";
+            // copy the text file inside the same folder with different name
+            string path1 = @"C:\Users\NITISH MEHTA\OneDrive\Desktop\Bridge_Labz\27\Addressbook_FileIO\test1.txt";
+            // To copy the file inside the folder 
+            string path2 = @"C:\Users\NITISH MEHTA\OneDrive\Desktop\Bridge_Labz\27\Addressbook_FileIO\Data\test2.txt";
+
+            // File.Copy(path, path2); for copy with differ file name or in differ folder
+            File.Copy(path, path2, true);
+            Console.ReadLine();
+
+        }
+    }
+}
